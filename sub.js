a07c799423b6c1dc564fc454584e0b4a001bf4c2
no7david <wdeng@kalengo.com>
2016-05-14 10:58:16 +0800

    comment

--- sub.js
+++ sub.js
@@ -1,7 +1,10 @@
 var redis = require("redis")
   , subscriber = redis.createClient();
-//注意跨进程接受，并且是广播类别
+//注意：跨进程接收，并且是广播类别
+//*********** 务必注意坑 **********
 //也就是说：1个pub n个sub 的结构，n个sub 都接收到，请务必注意逻辑
+//*********** 务必注意坑 **********
+
 subscriber.on("message", function(channel, message) {
   console.log(new Date() + "Message '" + message + "' on channel '" + channel + "' arrived!")
 });
