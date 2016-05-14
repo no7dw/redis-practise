fc3daf9bece2d988de260aea3d215462541d1565
no7david <wdeng@kalengo.com>
2016-05-14 10:53:13 +0800

    add comment

--- pub.js
+++ pub.js
@@ -3,14 +3,14 @@ var redis = require("redis")
   , publisher  = redis.createClient();
 
 Thenjs.each([1,2,3,4,5], function(cont, value){
-    //when no sub？
     //注意 always return false whether has a subscriber or not
     //注意 异步 of cause
+    //忽略返回的情况
     var res = publisher.publish("testEvent", "haaaaa"+ value);
     console.log(res);
     cont(null, res);
 }).then(function(cont ,result){
     console.log("end",result);
-    //when to die
-//    process.exit(0);
+    //not ready to die, must NOT exist now
+	//    process.exit(0);//this line of code should NOT run
 });
